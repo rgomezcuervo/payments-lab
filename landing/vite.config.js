2a9f9ed82d20b86799df78877684463fa8758fd4
Ricardo Gómez Cuervo <rgomezcuervo@Ricardos-MacBook-Pro.local>
2026-03-28 20:00:03 -0500

    added features and issues structure

--- landing/vite.config.js
+++ landing/vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  server: {
+    allowedHosts: [
+      '596d-186-28-168-110.ngrok-free.app' // Allow a specific hostname
+    ]
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
